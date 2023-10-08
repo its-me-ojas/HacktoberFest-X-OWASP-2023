6a118e63efef29f2435534c904ceac069d9c8800
Mankirat Singh <msingh2_be22@thapar.edu>
2023-10-08 10:28:24 +0530

    Change favicon, title and add binary stream element
    Signed-off-by: Mankirat Singh <msingh2_be22@thapar.edu>

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
 import './App.css'
-
+import { useState, useEffect } from 'react';
 function App() {
+  const [binaryStream, setBinaryStream] = useState(generateRandomBinaryStream());
+
+  // Function to generate a random binary stream
+  function generateRandomBinaryStream() {
+    return Array.from({ length: window.innerWidth }, () =>
+      Math.random() < 0.5 ? '0' : '1'
+    ).join('');
+  }
+
+  // Update the binary stream on a timer
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setBinaryStream(generateRandomBinaryStream());
+    }, 150); // Update every 100 milliseconds
+
+    return () => {
+      clearInterval(intervalId); // Cleanup when component unmounts
+    };
+  }, []);
+
   return (
-    <div className="App">
-      <h1>It works</h1>
+    <div className='binStream'>
+      {binaryStream}
     </div>
-  )
+  );
 }
 
 export default App
